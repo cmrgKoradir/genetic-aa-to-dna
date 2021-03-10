d6de5feb3cd42cbb1252aede820b03dfc9eab6d1
Roger Koradi <roger.koradi@comerge.net>
2021-03-10 23:11:06 +0100

    Add weightedPick helper function

--- random.js
+++ random.js
@@ -5,3 +5,19 @@
  export function randInt(upperBound){
     return Math.floor(Math.random() * upperBound)
 }
+
+/**
+ * Implements
+ * @param {any[]} items a collection to pick from
+ * @param weight a function from an item to its normalized weight
+ * @returns a random item based on the weights
+ */
+export function weightedPick(items, weight){
+    var r = Math.random()
+    for(const i of items){
+        r -= weight(i)
+        if (r < 0){
+            return i
+        }
+    }
+}
